3706ebd9933173d6fed89ac78fa6b35d23ad1b0f
Tania B <yalyna.ts@gmail.com>
2023-03-16 16:19:19 +0100

    Ensure we read from overlay when calling readDir

--- cue/load/fs.go
+++ cue/load/fs.go
@@ -167,6 +167,20 @@ func hasSubdir(root, dir string) (rel string, ok bool) {
 
 func (fs *fileSystem) readDir(path string) ([]os.FileInfo, errors.Error) {
 	path = fs.makeAbs(path)
+
+	if fi := fs.getDir(path, false); fi != nil {
+		items := make([]os.FileInfo, len(fi))
+		i := 0
+		for _, of := range fi {
+			items[i] = of
+			i++
+		}
+		sort.Slice(items, func(i, j int) bool {
+			return items[i].Name() < items[j].Name()
+		})
+		return items, nil
+	}
+	
 	m := fs.getDir(path, false)
 	items, err := ioutil.ReadDir(path)
 	if err != nil {
